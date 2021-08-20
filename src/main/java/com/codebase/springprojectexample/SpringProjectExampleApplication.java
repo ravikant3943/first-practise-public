13283642f2898f1678a15441057a3f7be6041b31
ravikant3943 <khobragade.ravikant@gmail.com>
2021-08-20 17:16:31 +0100

    Added Spring-example

---
+++ src/main/java/com/codebase/springprojectexample/SpringProjectExampleApplication.java
@@ -0,0 +1,16 @@
+package com.codebase.springprojectexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectExampleApplication {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Run First App");
+		SpringApplication.run(SpringProjectExampleApplication.class, args);
+	}
+
+}
